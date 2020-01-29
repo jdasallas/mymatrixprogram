c4988786836657e11374ea729eca19dec73d3661
jdasallas <jerine.dasallas@gmail.com>
2020-01-29 07:51:08 +0000

    code to open files from CLI arguments

--- mymatrixprogram.c
+++ mymatrixprogram.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//   read matrix files from CLI input
+//   read matrix files from CLI arguments
 
 int main(int argc, char * argv[])
 {
@@ -10,14 +10,7 @@ int main(int argc, char * argv[])
 	fptr1 = fopen(argv[1],"r");
 	fptr2 = fopen(argv[2],"r");
 
-	if( fptr1 == NULL || fptr2 == NULL )
-	{
-		perror("Error ");
-		printf("Press any key to exit...\n");
-		exit(0);
-		exit(EXIT_FAILURE);
-	}
-	fclose(fptr1);
-	fclose(fptr2);
+//	fclose(fptr1);
+//	fclose(fptr2);
 
 }
