d3841585acf1864b7ab77444e8816d8e6a58dddd
jdasallas <jerine.dasallas@gmail.com>
2020-01-29 17:22:14 +0800

    edited function

--- mymatrixprogram.c
+++ mymatrixprogram.c
@@ -9,16 +9,16 @@ int main(int argc, char * argv[])
 int i,j,k;
 int rows1,cols1,rows2,cols2;
 int sum=0;	
-	FILE *f1, *f2;
+	FILE *fp1, *fp2;
 
-	f1 = fopen("matrix1.txt","r");
-	f2 = fopen("mtarix2.txt","r");
+	fp1 = fopen(argv[1],"r");
+	fp2 = fopen(argv[2],"r");
 
 // count  rows and columns in each matrix
-fscanf(f1,"%i",&rows1);
-fscanf(f1,"%i",&cols1);
-fscanf(f2,"%i",&rows2);
-fscanf(f2,"%i",&cols2);
+fscanf(fp1,"%i",&rows1);
+fscanf(fp1,"%i",&cols1);
+fscanf(fp2,"%i",&rows2);
+fscanf(fp2,"%i",&cols2);
 
 printf("\n\nRows1 = %d",rows1);
 printf("\nCols1 = %d",cols1);
@@ -27,6 +27,6 @@ printf("\nCols2 = %d",cols2);
 
 
 // close the input files
-    fclose(f1);
-    fclose(f2);
+    fclose(fp1);
+    fclose(fp2);
 }
