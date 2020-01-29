47db1149ad305daa2583c72dfaca19781426f04d
jdasallas <jerine.dasallas@gmail.com>
2020-01-29 19:36:24 +0800

    added lines for matrix allocation

--- mymatrixprogram.c
+++ mymatrixprogram.c
@@ -1,32 +1,60 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include<math.h>
 
 //   read matrix files from CLI arguments
 
 int main(int argc, char * argv[])
 
 {
-int i,j,k;
-int rows1,cols1,rows2,cols2;
-int sum=0;	
-	FILE *fp1, *fp2;
+	int i,j,k;
+	int rows1,cols1,rows2,cols2;
+	int sum=0;	
+
+	FILE *fp1, *fp2, *matrixoutput;
 
 	fp1 = fopen(argv[1],"r");
 	fp2 = fopen(argv[2],"r");
 
-// count  rows and columns in each matrix
 
-fscanf(fp1,"%i",&rows1);
-fscanf(fp1,"%i",&cols1);
-fscanf(fp2,"%i",&rows2);
-fscanf(fp2,"%i",&cols2);
+// allocate for matrix1
 
-printf("\n\nRows1 = %d",rows1);
-printf("\nCols1 = %d",cols1);
-printf("\n\nRows2 = %d",rows2);
-printf("\nCols2 = %d",cols2);
+int** matrix1 = malloc(2*sizeof(int*));
+for (i=0; i < 2; i++)
+matrix1[i]=malloc(2*sizeof(int));
+	fp1 = fopen(argv[1],"r");
+
+	 for(i = 0; i < 2; i++)
+  {
+      for(j = 0; j < 2; j++) 
+      {
+       if (!fscanf(fp1, "%d", &matrix1[i][j])) 
+           break;
+      // mat[i][j] -= '0'; 
+       printf("%d\n",matrix1[i][j]); 
+      }
+
+  }
+
+// allocate for matrix2
+
+int** matrix2 = malloc(2*sizeof(int*));
+for (i=0; i < 2; i++)
+matrix2[i]=malloc(2*sizeof(int));
+	fp1 = fopen(argv[1],"r");
+
+	 for(i = 0; i < 2; i++)
+  {
+      for(j = 0; j < 2; j++) 
+      {
+       if (!fscanf(fp2, "%d", &matrix2[i][j])) 
+           break;
+      // mat[i][j] -= '0'; 
+       printf("%d\n",matrix2[i][j]); 
+      }
+
+  }
 
 
-// close the input files
-    fclose(fp1);
-    fclose(fp2);
+	return 0;
 }
